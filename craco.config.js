ba8f2ce74296a4fb180336689926cb8e2e91379c
Philipe <philipegatis@gmail.com>
2021-05-07 10:12:20 -0300

    add theme less for semantic

--- craco.config.js
+++ craco.config.js
@@ -2,6 +2,7 @@ const CracoAlias = require("craco-alias");
 
 module.exports = {
   plugins: [
+    { plugin: require('@semantic-ui-react/craco-less') },
     {
       plugin: CracoAlias,
       options: {
