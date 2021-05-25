207fe2be91602095485b06d81561771954787b73
Philipe <philipegatis@gmail.com>
2021-05-25 10:29:39 -0300

    socket

--- src/components/contents/StorieNew/index.tsx
+++ src/components/contents/StorieNew/index.tsx
@@ -3,26 +3,11 @@ import Upload from './Upload'
 import { useHistory, useLocation } from 'react-router-dom'
 import { parse, stringify } from 'qs'
 import { useSendFile } from './useSendFile'
+import { useFileStatus } from './useFileStatus'
 import { useRecoilValue } from 'recoil'
 import { fileIdState } from './state'
-import { StompSessionProvider, useSubscription } from 'react-stomp-hooks'
 
 export type Steps = 'videoDetails'
-
-const WS = function <T>(Component: React.ComponentType<T>) {
-  const getToken = () => {
-    const tokenItem = localStorage.getItem('token')
-    return tokenItem ? JSON.parse(tokenItem) : null
-  }
-
-  const ws = (props: T) => (
-    <StompSessionProvider url={`/api/ws?jwt=${getToken()}`}>
-      <Component {...props} />
-    </StompSessionProvider>
-  )
-
-  return ws
-}
 
 const StorieNew: React.FC = () => {
   const { pathname, search } = useLocation()
@@ -33,6 +18,7 @@ const StorieNew: React.FC = () => {
   const fileId = useRecoilValue(fileIdState)
 
   const { sendFile, progress } = useSendFile()
+  const { fileStatus } = useFileStatus()
 
   const nextStep = (nextStep: Steps) => {
     history.replace({
@@ -47,10 +33,6 @@ const StorieNew: React.FC = () => {
     })
   }
 
-  useSubscription('/user/queue/video-progress', (message: { body: string }) => {
-    console.log(JSON.parse(message.body))
-  })
-
   return (
     <>
       <Upload handleNext={nextStep} sendFile={sendFile} />
@@ -58,4 +40,4 @@ const StorieNew: React.FC = () => {
   )
 }
 
-export default WS(StorieNew)
+export default StorieNew
---
+++ src/components/contents/StorieNew/useFileStatus.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+import { Client } from '@stomp/stompjs'
+import SockeJS from 'sockjs-client'
+
+export const useFileStatus = () => {
+  const [fileStatus, setFileStatus] = useState<number>(0)
+  const [client] = useState<Client>(new Client())
+
+  const getToken = () => {
+    const tokenItem = localStorage.getItem('token')
+    return tokenItem ? JSON.parse(tokenItem) : null
+  }
+
+  client.configure({
+    webSocketFactory: () => new SockeJS(`/api/ws?jwt=${getToken()}`),
+    onConnect: () => {
+      client.subscribe('/user/queue/video-progress', (message: { body: string }) => {
+        console.log(JSON.parse(message.body))
+        setFileStatus(JSON.parse(message.body))
+      })
+    }
+  })
+
+  useEffect(() => {
+    client.activate()
+    return () => {
+      client.deactivate()
+    }
+  }, [])
+
+  return { fileStatus }
+}
