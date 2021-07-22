cdc3968b7be54f1238a20b711c7f3ab3212d31ea
Daniel Gatis <danielgatis@gmail.com>
2021-07-22 18:05:50 -0300

    comment box

--- src/components/contents/StoryShow/TabsContent/Info/index.tsx
+++ src/components/contents/StoryShow/TabsContent/Info/index.tsx
@@ -1,25 +1,13 @@
-import CustomMentionEditor from '@root/components/suport/CustomMentionEditor'
 import React from 'react'
 import { StoryType } from '@root/types'
+import CommentThread from '@root/components/suport/CommentThread'
 
 interface iInfo {
   story: Partial<StoryType>
 }
 
 const Info: React.FC<iInfo> = ({ story }) => {
-  const mentions = story.watchers
-    ?.filter(w => w.user)
-    .map(w => {
-      return {
-        name: w.user.firstName + ' ' + w.user.lastName,
-        avatar: w.user.picture?.thumbnail
-      }
-    })
-  return (
-    <div>
-      <CustomMentionEditor mentions={mentions} />
-    </div>
-  )
+  return <CommentThread story={story} />
 }
 
 export default Info
---
+++ src/components/suport/CommentThread/index.tsx
@@ -0,0 +1,94 @@
+import React, { ReactElement } from 'react'
+import { Button, Comment, Form, Header } from 'semantic-ui-react'
+import { StoryType } from '@root/types'
+import CustomMentionEditor from '@root/components/suport/CustomMentionEditor'
+
+interface iCommentThread {
+  story: Partial<StoryType>
+}
+
+const CommentThread: React.FC<iCommentThread> = ({ story }): ReactElement => {
+  const mentions = story.watchers
+    ?.filter(w => w.user)
+    .map(w => {
+      return {
+        name: w.user.firstName + ' ' + w.user.lastName,
+        avatar: w.user.picture?.thumbnail
+      }
+    })
+
+  return (
+    <Comment.Group threaded>
+      <Header as="h3" dividing>
+        Comments
+      </Header>
+
+      <Comment>
+        <Comment.Avatar as="a" src="https://react.semantic-ui.com/images/avatar/small/matt.jpg" />
+        <Comment.Content>
+          <Comment.Author as="a">Matt</Comment.Author>
+          <Comment.Metadata>
+            <span>Today at 5:42PM</span>
+          </Comment.Metadata>
+          <Comment.Text>How artistic!</Comment.Text>
+          <Comment.Actions>
+            <a>Reply</a>
+          </Comment.Actions>
+        </Comment.Content>
+      </Comment>
+
+      <Comment>
+        <Comment.Avatar as="a" src="https://react.semantic-ui.com/images/avatar/small/elliot.jpg" />
+        <Comment.Content>
+          <Comment.Author as="a">Elliot Fu</Comment.Author>
+          <Comment.Metadata>
+            <span>Yesterday at 12:30AM</span>
+          </Comment.Metadata>
+          <Comment.Text>
+            <p>This has been very useful for my research. Thanks as well!</p>
+          </Comment.Text>
+          <Comment.Actions>
+            <a>Reply</a>
+          </Comment.Actions>
+        </Comment.Content>
+
+        <Comment.Group>
+          <Comment>
+            <Comment.Avatar as="a" src="https://react.semantic-ui.com/images/avatar/small/jenny.jpg" />
+            <Comment.Content>
+              <Comment.Author as="a">Jenny Hess</Comment.Author>
+              <Comment.Metadata>
+                <span>Just now</span>
+              </Comment.Metadata>
+              <Comment.Text>Elliot you are always so right :)</Comment.Text>
+              <Comment.Actions>
+                <a>Reply</a>
+              </Comment.Actions>
+            </Comment.Content>
+          </Comment>
+        </Comment.Group>
+      </Comment>
+
+      <Comment>
+        <Comment.Avatar as="a" src="https://react.semantic-ui.com/images/avatar/small/joe.jpg" />
+        <Comment.Content>
+          <Comment.Author as="a">Joe Henderson</Comment.Author>
+          <Comment.Metadata>
+            <span>5 days ago</span>
+          </Comment.Metadata>
+          <Comment.Text>Dude, this is awesome. Thanks so much</Comment.Text>
+          <Comment.Actions>
+            <a>Reply</a>
+          </Comment.Actions>
+        </Comment.Content>
+      </Comment>
+
+      <Form reply>
+        <CustomMentionEditor mentions={mentions} />
+        <Button content="Add Reply" labelPosition="left" icon="edit" primary />
+      </Form>
+    </Comment.Group>
+  )
+}
+
+export default CommentThread
