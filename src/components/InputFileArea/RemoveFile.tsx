8595a293373483d87516f87120bebd736e03fc05
PhilipeGatis <philipegatis@gmail.com>
2022-04-19 20:42:44 -0300

    fix re-record button typo

--- src/components/InputFileArea/RemoveFile.tsx
+++ src/components/InputFileArea/RemoveFile.tsx
@@ -50,7 +50,7 @@ const RemoveFile: React.FC<RemoveFile> = ({ file, onRemove, quotaError, onSubscr
         </Typography>
         <Box flex={1} />
         <Box>
-          <Button onClick={onRemove}>Re-recorder</Button>
+          <Button onClick={onRemove}>Re-record</Button>
         </Box>
       </Box>
       <Player maxHeight="520px" subtitles={[]} type={'VIDEO'} media={{ id: 'local', url: URL.createObjectURL(file) }} />
